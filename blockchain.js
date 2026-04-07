8bd574195db709a7cf8d774d9251602313fe8f0f
Puncak Bukit <26822588+puncakbukit@users.noreply.github.com>
2026-04-08 07:35:44 +0900

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -11,10 +11,10 @@ const APP_URL = "https://puncakbukit.github.io/steembiota";
 // ---- RPC nodes & fallback ----
 
 const RPC_NODES = [
-  "https://api.steemit.com",
-  "https://api.justyy.com",
-  "https://steemd.steemworld.org",
-  "https://api.steem.fans"
+  "https://api.steemit.com"
+  // "https://api.justyy.com",
+  // "https://steemd.steemworld.org",
+  // "https://api.steem.fans"
 ];
 
 let currentRPCIndex = 0;
